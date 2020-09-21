f499dd17b313986f7c16dc9906280be0b8d5828d
juampifortu <juampifortunato98@gmail.com>
2020-09-21 12:52:38 -0300

    Agrego resolucion completa del ejercicio

---
+++ ABM_Alimento/Controllers/AlimentosController.cs
@@ -0,0 +1,51 @@
+﻿using Entidades;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABM_Alimento.Controllers
+{
+    public class AlimentosController : Controller
+    {
+        AlimentoServicio alimentoServicio = new AlimentoServicio();
+        // GET: Alimento
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Alta()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DarAlta(Alimento alimento)
+        {
+            alimentoServicio.Alta(alimento);
+            return RedirectToAction("Todos");
+        }
+        public ActionResult Todos()
+        {
+            List<Alimento> alimentos = alimentoServicio.Todos();
+            return View(alimentos);
+        }
+        public ActionResult Editar(int id)
+        {
+            Alimento alimento = alimentoServicio.buscarPorId(id);
+            return View(alimento);
+        }
+        [HttpPost]
+        public ActionResult Editar(Alimento alimento)
+        {
+            alimentoServicio.Editar(alimento);
+            return RedirectToAction("Todos");
+        }
+        public ActionResult Borrar(int id)
+        {
+            alimentoServicio.Borrar(id);
+            return RedirectToAction("Todos");
+        }
+    }
+}
---
+++ Servicios/AlimentoServicio.cs
@@ -0,0 +1,44 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class AlimentoServicio
+    {
+        public static List<Alimento> alimentos = new List<Alimento>();
+
+        public void Alta(Alimento alimento)
+        {
+            int maxId = 0;
+            if (alimentos.Count > 0)
+            {
+                maxId = alimentos.Max(o => o.id);
+            }
+            alimento.id = maxId + 1;
+            alimentos.Add(alimento);
+        }
+        public List<Alimento> Todos()
+        {
+            return alimentos;
+        }
+        public void Editar(Alimento alimento)
+        {
+            Alimento ali = buscarPorId(alimento.id);
+            ali.nombre = alimento.nombre;
+            ali.peso = alimento.peso;
+        }
+        public Alimento buscarPorId(int id)
+        {
+            return alimentos.Find(o => o.id == id);
+        }
+        public void Borrar(int id)
+        {
+            alimentos.RemoveAll(o => o.id == id);
+        }
+    }
+
+}
